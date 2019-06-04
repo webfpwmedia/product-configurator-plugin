90179e0561e414bf42ce956baa0e4bdc72d17635
Kevin DeCapite <kevin@arcpointgroup.com>
2019-06-04 09:27:34 -0700

    Added route.

--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,6 @@ use Cake\Routing\Route\DashedRoute;
 use Cake\Routing\Router;
 use Cake\Routing\RouteBuilder;
 
-Router::plugin('Arc\ProductConfigurator', ['path' => '/arc-product-configurator'], function (RouteBuilder $routes) {
+Router::plugin('ARC/ProductConfigurator', ['path' => '/arc-product-configurator'], function (RouteBuilder $routes) {
     $routes->fallbacks(DashedRoute::class);
 });
