ec6a6670a846594a80bc4897f9bf3dc069c59d42
Kevin DeCapite <kevin@arcpointgroup.com>
2019-06-04 15:26:15 -0700

    Factoring config.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,7 +3,7 @@
 use Cake\Core\Configure;
 use Cake\Datasource\ConnectionManager;
 
-Configure::load('ARC.ProductConfigurator', 'config', true);
+Configure::load('ARC.ProductConfigurator', 'default', true);
 
 ConnectionManager::setConfig('configurator', [
     'url' => 'mysql://root:root@mysql/configurator?encoding=utf8&timezone=UTC&cacheMetadata=true&quoteIdentifiers=false&persistent=false',
--- config/config.php
+++ config/config.php
@@ -1,23 +1,19 @@
 <?php
 
 return [
-    'ARC' => [
-        'ProductConfigurator' => [
-            'common' => [
-                'copyright' => 'Arc Point Group',
-                'name' => 'Product Configurator',
-                'website' => 'https://www.arcpointgroup.com/',
-            ],
+    'common' => [
+        'copyright' => 'Arc Point Group',
+        'name' => 'Product Configurator',
+        'website' => 'https://www.arcpointgroup.com/',
+    ],
 
-            'date' => [
-                'default' => 'F j, Y g:ia',
-            ],
+    'date' => [
+        'default' => 'F j, Y g:ia',
+    ],
 
-            'imageBaseUrl' => 'img/',
+    'imageBaseUrl' => 'img/',
 
-            'meta' => [
-                'description' => 'Arc Point Group product configurator plugin for CakePHP.',
-            ],
-        ],
+    'meta' => [
+        'description' => 'Arc Point Group product configurator plugin for CakePHP.',
     ],
 ];
