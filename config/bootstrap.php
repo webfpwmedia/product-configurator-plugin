149629acd5df6cedc930d8314805e7e8ef0867a3
Kevin DeCapite <kevin@arcpointgroup.com>
2019-06-04 13:17:53 -0700

    Updating image path.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -14,14 +14,17 @@ $config = [
         'default' => 'F j, Y g:ia',
     ],
 
-    'imageBaseUrl' => 'img/',
-
     'meta' => [
         'description' => 'Arc Point Group product configurator plugin for CakePHP.',
     ],
 ];
 
 Configure::write('ARC.ProductConfigurator', $config);
+
+# Allow implementing app to specify image path.
+if (!Configure::read('ARC.ProductConfigurator.imageBaseUrl')) {
+    Configure::write('ARC.ProductConfigurator.imageBaseUrl', 'img/');
+}
 
 ConnectionManager::setConfig('configurator', [
     'url' => 'mysql://root:root@mysql/configurator?encoding=utf8&timezone=UTC&cacheMetadata=true&quoteIdentifiers=false&persistent=false',
