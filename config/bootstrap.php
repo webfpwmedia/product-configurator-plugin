3fd060029209802ecd4083938518031eb1ed4014
Jeremy Harris <jeremy@someguyjeremy.com>
2019-06-27 11:49:00 -0500

    Renamed userConfiguration to appConfiguration

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,7 +4,7 @@ use Cake\Core\Configure;
 use Cake\Utility\Hash;
 
 // merge user configuration
-$userConfiguration = Configure::read('ARC.ProductConfigurator');
+$appConfiguration = Configure::read('ARC.ProductConfigurator');
 Configure::load('ARC/ProductConfigurator.config');
 $config = Configure::read('ARC.ProductConfigurator');
-Configure::write('ARC.ProductConfigurator', Hash::merge($config, $userConfiguration));
+Configure::write('ARC.ProductConfigurator', Hash::merge($config, $appConfiguration));
